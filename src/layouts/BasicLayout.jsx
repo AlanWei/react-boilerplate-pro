1306bf54e08ff59cda20ecd219b8a1d23f23babe
Alan Wei <alan.wei@shopback.com>
2018-05-28 16:13:11 +0800

    tweak basicLayout

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
-import { Avatar, Dropdown, Menu } from 'antd';
+import { injectIntl } from 'react-intl-context';
+import { Avatar, Dropdown, Menu, Icon } from 'antd';
 import Sider from 'react-sider';
 import 'react-sider/lib/index.css';
 import menuData from 'app/config/menu';
@@ -19,6 +20,8 @@ const propTypes = {
   className: PropTypes.string,
   location: PropTypes.object.isRequired,
   user: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+  intl: PropTypes.object.isRequired,
 };
 
 const defaultProps = {
@@ -28,25 +31,37 @@ const defaultProps = {
 
 class BasicLayout extends Component {
   renderHeader = () => {
+    const {
+      logout,
+      prefixCls,
+      user,
+      intl,
+    } = this.props;
+
     const menu = (
       <Menu>
-        <Menu.Item>
+        <Menu.Item disabled className={`${prefixCls}-userMenuItem`}>
+          <Icon type="user" className={`${prefixCls}-userMenuIcon`} />
+          <span>{intl.formatMessage({ id: 'basicLayout_profile' })}</span>
+        </Menu.Item>
+        <Menu.Item className={`${prefixCls}-userMenuItem`}>
           <div
-            onClick={this.props.logout}
+            onClick={logout}
             role="presentation"
           >
-            Logout
+            <Icon type="logout" className={`${prefixCls}-userMenuIcon`} />
+            <span>{intl.formatMessage({ id: 'basicLayout_logout' })}</span>
           </div>
         </Menu.Item>
       </Menu>
     );
 
     return (
-      <div className="globalHeader">
+      <div className={`${prefixCls}-header`}>
         <Dropdown overlay={menu} placement="bottomRight">
-          <div className="globalHeader-avatar">
+          <div className={`${prefixCls}-avatar`}>
             <Avatar>
-              {getFirstChar(this.props.user.name)}
+              {getFirstChar(user.name)}
             </Avatar>
           </div>
         </Dropdown>
@@ -55,28 +70,36 @@ class BasicLayout extends Component {
   }
 
   renderFooter = () => (
-    <div className="globalFooter">
+    <div className={`${this.props.prefixCls}-footer`}>
       Copyright © 2018
     </div>
   )
 
   render() {
+    const {
+      prefixCls,
+      className,
+      location,
+      children,
+    } = this.props;
+
     const classes = classnames({
-      [this.props.prefixCls]: true,
-      [this.props.className]: true,
+      [prefixCls]: true,
+      [className]: true,
     });
+
     return (
       <div className={classes}>
         <Sider
           appName={appName}
           appLogo={logo}
           menuData={menuData}
-          pathname={this.props.location.pathname}
+          pathname={location.pathname}
         />
-        <div className="content">
+        <div className={`${prefixCls}-content`}>
           {this.renderHeader()}
-          <div className="mainContent">
-            {this.props.children}
+          <div className={`${prefixCls}-mainContent`}>
+            {children}
           </div>
           {this.renderFooter()}
         </div>
@@ -95,4 +118,7 @@ const mapDispatchToProps = {
 
 BasicLayout.propTypes = propTypes;
 BasicLayout.defaultProps = defaultProps;
-export default connect(mapStateToProps, mapDispatchToProps)(BasicLayout);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(injectIntl(BasicLayout));
